e4eeb14197e374c827c889db9e9f06d6c946e220
Shiju Varghese <shiju.varghese@outlook.com>
2016-06-26 08:49:24 +0530

    Slice example

--- ch02/slices/main.go
+++ ch02/slices/main.go
@@ -5,30 +5,32 @@ import (
 )
 
 func main() {
-	//	//	x := make([]int, 3, 5)
-	//	//	x[0] = 10
-	//	//	x[1] = 20
-	//	//	x[2] = 30
-	//	//	fmt.Println(x)
+	x := make([]int, 3, 5)
+	x[0] = 10
+	x[1] = 20
+	x[2] = 30
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 
-	//	y := make([]int, 3, 5)
-	//	y[0] = 10
-	//	y[1] = 20
-	//	y[2] = 30
-	//	fmt.Println(y)
-	//	fmt.Println(len(y))
-	//	fmt.Println(len(y))
+	y := make([]int, 3)
+	y[0] = 10
+	y[1] = 20
+	y[2] = 30
+	fmt.Println(y)
+	fmt.Println(len(y))
+	fmt.Println(cap(y))
 
-	//	//	z := []int{10, 20, 30}
-	//	//	fmt.Println(len(z))
-	//	//	fmt.Println(len(z))
+	z := []int{10, 20, 30}
+	fmt.Println(len(z))
+	fmt.Println(cap(z))
 
-	//	z := []int{0: 10, 2: 30}
-	//	fmt.Println(len(z))
-	//	fmt.Println(len(z))
+	z1 := []int{0: 10, 2: 30}
+	fmt.Println(len(z1))
+	fmt.Println(cap(z1))
 
-	x := []int{10, 20, 30}
-	y := append(x, 40, 50)
-	fmt.Println(x, y)
+	x1 := []int{10, 20, 30}
+	y1 := append(x1, 40, 50)
+	fmt.Println(x1, y1)
 
 }
