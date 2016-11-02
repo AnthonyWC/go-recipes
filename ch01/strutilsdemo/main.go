b72ef3d0a580838c78756f6df349ed8772c47598
Shiju Varghese <shiju.varghese@outlook.com>
2016-11-02 22:21:51 +0530

    Added example for binary only package

--- ch01/strutilsdemo/main.go
+++ ch01/strutilsdemo/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("To Upper Case:", strutils.ToUpperCase(str1))
 
 	// Convert to lower case
-	fmt.Println("To Lower Case:", strutils.ToUpperCase(str1))
+	fmt.Println("To Lower Case:", strutils.ToLowerCase(str1))
 
 	// Convert first letter to upper case
 	fmt.Println("To First Upper:", strutils.ToFirstUpper(str2))
